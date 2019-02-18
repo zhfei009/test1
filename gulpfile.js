53272cb04029ae2b54bae886ddc9e9ca3433bf21
zfei <zhfei_cz@163.com>
2019-02-18 11:49:14 +0800

    完成

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var webserver = require('gulp-webserver');
 var fs = require('fs');
 var path = require('path');
 var url = require('url');
+var babel = require('gulp-babel');
 
 // bianyisass
 gulp.task('scss', function() {
@@ -14,7 +15,20 @@ gulp.task('scss', function() {
         .pipe(gulp.dest('./src/css'))
 })
 
+// 编译js
+gulp.task('byjs', function() {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('./src/js/babel'))
+})
+
 // 监测scss
+gulp.task('conscss', function() {
+    gulp.watch(['./src/scss/**/*.scss', './src/js/**/*.js'], gulp.series('scss', 'byjs'))
+})
+
 
 // 启服务
 gulp.task('server', function() {
@@ -25,8 +39,18 @@ gulp.task('server', function() {
             livereload: true,
             middleware: function(req, res, next) {
                 var pname = url.parse(req.url).pathname;
-                console.log(pname)
-                next();
+                if (pname === '/favicon.ico') {
+                    return res.end()
+                }
+                if (pname === '/asds') {
+                    res.end(JSON.stringify({ code: 1 }))
+                } else {
+                    pname = pname === '/' ? 'index.html' : pname;
+                    res.end(fs.readFileSync(path.join(__dirname, 'src', pname)));
+                }
             }
         }))
-})
+});
+
+// 管理
+gulp.task('default', gulp.series('scss', 'byjs', 'server', 'conscss'));
